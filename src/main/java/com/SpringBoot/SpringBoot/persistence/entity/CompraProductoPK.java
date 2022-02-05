42449ef97375de2fdb65cc31ccb0081e4435cf29
Jdavid198 <jdavid198.jdr@gmail.com>
2022-02-05 16:24:45 -0600

    este es nuevo proceso

--- src/main/java/com/SpringBoot/SpringBoot/persistence/entity/CompraProductoPK.java
+++ src/main/java/com/SpringBoot/SpringBoot/persistence/entity/CompraProductoPK.java
@@ -1,2 +1,30 @@
-package com.SpringBoot.SpringBoot.persistence.entity;public class CompraProductoPK {
+package com.SpringBoot.SpringBoot.persistence.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import java.io.Serializable;
+
+@Embeddable
+public class CompraProductoPK implements Serializable {
+
+    @Column(name = "id_compra")
+    private Integer idCompra;
+    @Column(name = "id_producto")
+    private Integer idProducto;
+
+    public Integer getIdCompra() {
+        return idCompra;
+    }
+
+    public void setIdCompra(Integer idCompra) {
+        this.idCompra = idCompra;
+    }
+
+    public Integer getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(Integer idProducto) {
+        this.idProducto = idProducto;
+    }
 }
